77cabef3e3388666a400dad5616fb3555fd39a7c
ANKANA-MONDAL-MCA-A-2024-26-09-UEMK <mondalankana54@gmail.com>
2025-04-28 19:04:30 +0530

    28/04/2025

---
+++ week6/DisjointSet.py
@@ -0,0 +1,31 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])  # Path Compression
+        return self.parent[x]
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+
+        if root_x != root_y:
+            if self.rank[root_x] < self.rank[root_y]:
+                self.parent[root_x] = root_y
+            elif self.rank[root_x] > self.rank[root_y]:
+                self.parent[root_y] = root_x
+            else:
+                self.parent[root_y] = root_x
+                self.rank[root_x] += 1
+
+# Example Usage
+uf = UnionFind(5)
+uf.union(0, 1)
+uf.union(1, 2)
+uf.union(3, 4)
+print("Parent array after unions:", uf.parent)
+print("Find 2:", uf.find(2))
+print("Find 4:", uf.find(4))
